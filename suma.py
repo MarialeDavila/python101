86b292769e5ea8c5874167c0baa13a338ce55f54
Maria Alejandra Davila Salazar <mads519@gmail.com>
2015-05-08 18:04:04 -0500

    Update suma.py

--- suma.py
+++ suma.py
@@ -1,16 +1,9 @@
 def suma(a, b):
-    if a == 1 and b == 1:
-        return 2
-    else:
-        return a
-        
+    return a + b
         
   if a==2 and b==3
         return 5
      
-     
-    
- 
 def prueba():
     assert suma(1,0) == 1
     assert suma(0,0) == 0
